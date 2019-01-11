ea5f132fe8f5740a87eaa477f3157ab3035fd272
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-01-10 23:00:26 -0500

    added basic materialize styling to index.html, included jQuery CDN, and renamed frontend js file

---
+++ assets/javascript/frontend.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+
+    // init Sidenav
+    $('.sidenav-trigger').sidenav();
+    $('.sidenav').sidenav('edge', "left");
+    $('.sidenav').sidenav('inDuration', "250");
+    $('.sidenav').sidenav('outDuration', "250");
+
+});
