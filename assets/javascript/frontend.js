787a7a80aa92629e750d0cb4065df5502540fedb
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-01-15 18:48:07 -0500

    added in materialize tab component to switch between map/itinerary view

--- assets/javascript/frontend.js
+++ assets/javascript/frontend.js
@@ -1,8 +1,7 @@
 
 
 $(document).ready(function(){
-
-
+    $('.tabs').tabs();
 });
 
 // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAc37ceea-riBN1RsG5S-EX83UVoK-7W58&input=national+park+georgia&inputtype=textquery
