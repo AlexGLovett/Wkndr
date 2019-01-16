e5f590bd2f765cabdb62472e9b33c1df5154d2f0
antdroid23 <princemorrison23@yahoo.com>
2019-01-16 05:58:57 -0500

    started new survey. added cards and questions

--- assets/javascript/survey.js
+++ assets/javascript/survey.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     $('select').formSelect();
   });
-        
+
+
+//   $(".allownumericwithdecimal").on("keypress keyup blur",function (event) {
+//     //this.value = this.value.replace(/[^0-9\.]/g,'');
+// $(this).val($(this).val().replace(/[^0-9\.]/g,''));
+//     if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+//         event.preventDefault();
+//     }
+// });
+
+$('.numbers').keypress(function(key) {
+    if(key.charCode < 48 || key.charCode > 57) return false;
+});
