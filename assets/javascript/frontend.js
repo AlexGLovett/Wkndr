9efe9e1a9434f2be9c1cdca770c90ab607d26a4b
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-01-16 20:33:10 -0500

    results.html: added sidenav, rewrote main nav, completed basic layout, prototyped colors, added styling to several elements, attempted a background image, added CSS loading animation to map tab.

--- assets/javascript/frontend.js
+++ assets/javascript/frontend.js
@@ -1,7 +1,16 @@
-
-
 $(document).ready(function(){
+    // initializes sidenav and adds options
+    var elem = document.querySelector('.sidenav');
+    var instance = M.Sidenav.init(elem, {
+        inDuration: 350,
+        outDuration: 350,
+        edge: 'left'
+    });    
+
+    // initializes tabs
     $('.tabs').tabs();
+
+
 });
 
 // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAc37ceea-riBN1RsG5S-EX83UVoK-7W58&input=national+park+georgia&inputtype=textquery
