82c3868657d7640acb41536d6382c15ab40b58fb
Jayrene1 <43413926+Jayrene1@users.noreply.github.com>
2019-01-19 08:42:45 -0500

    messing with mapp.js

--- assets/javascript/mapp.js
+++ assets/javascript/mapp.js
@@ -1,15 +1,4 @@
-// 1. initialize firebase
-  var config = {
-    apiKey: "AIzaSyBPXBq7lkhMN8u7aRJOqok8zhZcElsFYI4",
-    authDomain: "wkndr-228415.firebaseapp.com",
-    databaseURL: "https://wkndr-228415.firebaseio.com",
-    projectId: "wkndr-228415",
-    storageBucket: "",
-    messagingSenderId: "75810052745"
-  };
-  firebase.initializeApp(config);
-
-// 2. global variables
+// global variables
     var map;
     var service;
     var infowindow;
@@ -54,25 +43,26 @@
     var lastDestination;
     var directionsService;
     var directionsDisplay;
+    var storedData;
+    var surveyData;
+    var tripLength;
+    var dist;
+    var loc;
 
-/* 3. initialize map object
-    Using Places and other Google Maps APIs through a Google Map Object
-    https://developers.google.com/maps/documentation/javascript/importing_data        
-*/
+$(document).ready(function() {
+// gather input data and initialize map object
 
-    //Initialize the map object from which other api calls are made
-    /*
-    Retrieving from local storage
-    */
     //get the data from local storage and parse to obj
-    var storedData = localStorage.getItem("survey");
-    var surveyData = JSON.parse(storedData);
-    //set survey variables
-    var terms = surveyData.types;
-    var tripLength = surveyData.tripLength;
-    var dist = surveyData.distance;
-    var loc = surveyData.location;
-    console.log(tripLength);
+    function getLocalStorage() {
+        storedData = localStorage.getItem("survey");
+        surveyData = JSON.parse(storedData);
+        //set survey variables
+        terms = surveyData.types;
+        tripLength = surveyData.tripLength;
+        dist = surveyData.distance;
+        loc = surveyData.location;
+    }
+
     function initMap() {
         
         //on map creation, center on Atlanta via lat-long coordinates
@@ -122,8 +112,8 @@
         }
     }
 
-    //When the search button is clicked, begin searching for selected terms within a distance of the origin point (zipcode)
-    $(document).ready(function() {
+    function beginSearch() {
+        getLocalStorage();
 
         primeTerm = terms[0];
 
@@ -139,7 +129,14 @@
 
         //Find the input search address, display it on the map, and perform a Places search within the area as a callback function
         codeAddress(loc,selectPrimeDestination);
+    }
+
+    beginSearch();
+
+    $("#planWeekend").on("click", function(){
+        beginSearch();
     });
+
 
     function codeAddress(location, callback) {
         var address = location;
@@ -154,7 +151,7 @@
                 callback();
             } 
             else {
-                alert('Geocode was not successful for the following reason: ' + status);
+                console.log('Geocode was not successful for the following reason: ' + status);
             }
         });
     }
@@ -366,7 +363,7 @@
               if (status === 'OK') {
                 directionsDisplay.setDirections(response);
               } else {
-                window.alert('Directions request failed due to ' + status);
+                console.log('Directions request failed due to ' + status);
               }
             });
         }
@@ -384,183 +381,4 @@
         }
       }
 
-
-    //PLACES TYPES
-    /*
-    accounting
-    airport
-    amusement_park
-    aquarium
-    art_gallery
-    atm
-    bakery
-    bank
-    bar
-    beauty_salon
-    bicycle_store
-    book_store
-    bowling_alley
-    bus_station
-    cafe
-    campground
-    car_dealer
-    car_rental
-    car_repair
-    car_wash
-    casino
-    cemetery
-    church
-    city_hall
-    clothing_store
-    convenience_store
-    courthouse
-    dentist
-    department_store
-    doctor
-    electrician
-    electronics_store
-    embassy
-    fire_station
-    florist
-    funeral_home
-    furniture_store
-    gas_station
-    gym
-    hair_care
-    hardware_store
-    hindu_temple
-    home_goods_store
-    hospital
-    insurance_agency
-
-    jewelry_store
-    laundry
-    lawyer
-    library
-    liquor_store
-    local_government_office
-    locksmith
-    lodging
-    meal_delivery
-    meal_takeaway
-    mosque
-    movie_rental
-    movie_theater
-    moving_company
-    museum
-    night_club
-    painter
-    park
-    parking
-    pet_store
-    pharmacy
-    physiotherapist
-    plumber
-    police
-    post_office
-    real_estate_agency
-    restaurant
-    roofing_contractor
-    rv_park
-    school
-    shoe_store
-    shopping_mall
-    spa
-    stadium
-    storage
-    store
-    subway_station
-    supermarket
-    synagogue
-    taxi_stand
-    train_station
-    transit_station
-    travel_agency
-    veterinary_care
-    zoo
-
-
-    */
-    // var map;
-    // function initMap() {
-    // map = new google.maps.Map(document.getElementById('map'), {
-    //     zoom: 2,
-    //     center: new google.maps.LatLng(2.8,-187.3),
-    //     //mapTypeId: 'terrain'
-    // });
-
-    // // Create a <script> tag and set the USGS URL as the source.
-    // var script = document.createElement('script');
-    //     // This example uses a local copy of the GeoJSON stored at
-    //     // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-    //     //cript.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    //     script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-    //     document.getElementsByTagName('head')[0].appendChild(script);
-    // }
-
-    // // Loop through the results array and place a marker for each
-    // // set of coordinates.
-    // window.eqfeed_callback = function(results) {
-    //     for (var i = 0; i < results.features.length; i++) {
-    //         var coords = results.features[i].geometry.coordinates;
-    //         var latLng = new google.maps.LatLng(coords[1],coords[0]);
-    //         var marker = new google.maps.Marker({
-    //         position: latLng,
-    //         map: map
-    //         });
-    //     }
-    // }
-
-
-
-/* 4. places logic -- https://developers.google.com/maps/documentation/javascript/places
-
-    Test Case 1:
-      Start Location: 714 Holmes Street, Atlanta 30318
-      Max Distance: 100 miles
-      Max Time: 2 days
-    
-    
-    a. pull user preferences or defaults from firebase db
-    b. based on number of available activities, create x arrays of y objects (ex: 5 nature activities, two food activities)
-    c. for each category of arrays, create maps search for the first point ~X miles away from the origin (100 miles if that's the users preference)
-    d. populate the category array of the activity with that activity's details, then plot another origin at activity 1
-    e. for each category of arrays, create a map search for the first point within ~Y miles away from origin 2 (~25-40 miles from origin at max), and populate the remaining empty objects in the category arrays with the top-rated or pseudorandom choices from response object JSON
-    f. create an dictionary of the itinerary points with the activity number as the key and the activity details, including inputs for the routing and viewing api calls as the values -- https://developers.google.com/maps/documentation/javascript/directions
-        
-          *sample call:
-                    {
-                origin: 'Chicago, IL',
-                destination: 'Los Angeles, CA',
-                waypoints: [
-                    {
-                    location: 'Joplin, MO',
-                    stopover: false
-                    },{
-                    location: 'Oklahoma City, OK',
-                    stopover: true
-                    }],
-                provideRouteAlternatives: false,
-                travelMode: 'DRIVING',
-                drivingOptions: {
-                    departureTime: new Date( now, or future date ),
-                    trafficModel: 'pessimistic'
-                },
-                unitSystem: google.maps.UnitSystem.IMPERIAL
-                }
-
-
-*/
-    
-
-/* 5. send places results to a list for display (itinerary) 
-*/
-
-
-/* 6. add places as markers to map
-*/
-
-
-/* 7. generate map route to destinations 
-*/
-      
+});
